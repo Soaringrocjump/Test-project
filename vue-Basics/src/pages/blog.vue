7c5f893f2bf36ee908f8bd2e9ba407ebbf637d78
Soaringrocjump <[775603374@qq.com]>
2019-11-04 17:29:17 +0800

    随便改改

--- vue-Basics/src/pages/blog.vue
+++ vue-Basics/src/pages/blog.vue
@@ -1,10 +1,11 @@
-//blog.vue
+// blog.vue
 <template>
   <div class="blog">
     <h1>我是blog页面</h1>
     <div class="blog-btn">
         <a class="btn" href="/">点击调到子路由1</a>
         <a class="btn" href="/info">点击调到子路由2</a>
+        <a class="btn">点击没用</a>
     </div>
   </div>
 </template>

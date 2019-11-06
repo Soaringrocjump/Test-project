ec3bc0353d88d9d2fc2c8e0b7857131b82aa7d63
Soaringrocjump <[775603374@qq.com]>
2019-11-06 10:19:57 +0800

    revert3

---
+++ develop/revert3.vue
@@ -0,0 +1,15 @@
+<!-- revert3 -->
+<template>
+  <div class="Vue">
+    This is a Vue component.
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
